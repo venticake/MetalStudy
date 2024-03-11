baa442e13d8b49566d6c7af6d8a45cd7ff552967
Sangwon Park <tastyone@gmail.com>
2024-03-11 10:39:16 +0900

    first import

---
+++ MetalStudy/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//
+
+import UIKit
+import Metal
+
+class ViewController: UIViewController {
+    var device: MTLDevice!
+    var metalLayer: CAMetalLayer!
+    let vertexData: [Float] = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0
+    ]
+    var vertexBuffer: MTLBuffer!
+    var pipelineState: MTLRenderPipelineState!
+    var commandQueue: MTLCommandQueue!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        device = MTLCreateSystemDefaultDevice()
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = .bgra8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        view.layer.addSublayer(metalLayer)
+        
+        let dataSize = vertexData.count * MemoryLayout.size(ofValue: vertexData[0])
+        vertexBuffer = device.makeBuffer(bytes: vertexData, length: dataSize, options: [])
+        
+        let defaultLibrary = device.makeDefaultLibrary()!
+        let fragmentProgram = defaultLibrary.makeFunction(name: "basic_fragment")
+        let vertexProgram = defaultLibrary.makeFunction(name: "basic_vertex")
+        
+        let pipelineStateDescriptor = MTLRenderPipelineDescriptor()
+        pipelineStateDescriptor.vertexFunction = vertexProgram
+        pipelineStateDescriptor.fragmentFunction = fragmentProgram
+        pipelineStateDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
+        
+        pipelineState = try! device.makeRenderPipelineState(descriptor: pipelineStateDescriptor)
+        
+        commandQueue = device.makeCommandQueue()
+    }
+}
+
